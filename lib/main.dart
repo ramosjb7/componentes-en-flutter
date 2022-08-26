c4023fb13446311b5490031d5c26208778e92fe2
brayan <br4y47.r4m0s@gmail.com>
2022-08-26 18:32:07 -0400

    Creando el proyecto Componentes flutter

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      //initialRoute: AppRoutes.getAppRoutes(),
+      //onGenerateInitialRoutes: AppRoutes.onGenerateRoute,
+      //theme: AppTheme.lightTheme,
+    );
+  }
+}
